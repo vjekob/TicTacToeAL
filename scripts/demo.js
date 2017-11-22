a0a8931b0d000e0d7f46361d23cc96d359a5bf94
Vjekoslav Babić <vjeko@vjeko.com>
2017-11-22 08:57:06 +0000

    Update demo.js

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,12 +1,31 @@
 // This file contains the actual JavaScript logic
 
-var demo = (function() {
-    var host;
+var demo = (function () {
+    var host, table;
+    var grid = {};
 
     var demo = {
-        init: function(id) {
+        init: function (id) {
             host = $("#" + id);
-            host.html("Control Loaded");
+            table = $("<table>");
+            host.append(table);
+
+            for (var row = 0; row < 3; row++) {
+                grid[row] = {
+                    $: $("<tr>"),
+                    index: row,
+                    cols: {}
+                };
+                table.append(grid[row].$);
+                for (var col = 0; col < 3; col++) {
+                    grid[row].cols[col] = {
+                        $: $("<td>"),
+                        index: col
+                    };
+                    grid[row].$.append(grid[row].cols[col].$);
+                    grid[row].cols[col].$.html("(" + col + ", " + row + ")");
+                };
+            };
         }
     };
 

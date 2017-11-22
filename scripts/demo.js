63b4a93e763e62d280218b20b4302822818fa1db
Vjekoslav Babić <vjeko@vjeko.com>
2017-11-22 08:09:31 +0000

    Step1

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,1 +1,14 @@
 // This file contains the actual JavaScript logic
+
+var demo = (function() {
+    var host;
+
+    var demo = {
+        init: function(id) {
+            host = $("#" + id);
+            host.html("Control Loaded");
+        }
+    };
+
+    return demo;
+})();
--- scripts/start.js
+++ scripts/start.js
@@ -1,1 +1,3 @@
 // This file contains the initialization logic
+
+demo && demo.init("controlAddIn");
